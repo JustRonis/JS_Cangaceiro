16e75ccdc3f9b9a2cef1a5ae798dec134bac771f
Ronaldo Luiz Pinto Filho <rpfilho@viajanet.com.br>
2021-11-26 10:11:30 -0300

    Subindo estrutura inicial, tela de negociações, classe negociacao

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,24 @@
+class Negociacao{
+    constructor(data, quantidade, valor){
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+
+    }
+
+    obtemVolume(){
+        return this._quantidade * this._valor;
+    }
+};
+
+
+this.quantidade
+
+abs = this.quantida > 10 ? "sim" : "não"
+
+if(abs === "sim"){
+    let a 
+    async () => {
+        await fs.writeFile("teste.txt", `${a}`)
+    }
+}
